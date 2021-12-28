44a4b2e7a4e05ac640c7e766e528b9414e19b8de
Siarhei Marzan <siarhei.marzan@gmail.com>
2021-12-28 13:32:40 +0300

    code: updated

--- src/test/java/test/ParameterizedTestTemperatureCheck.java
+++ src/test/java/test/ParameterizedTestTemperatureCheck.java
@@ -22,20 +22,19 @@ public class ParameterizedTestTemperatureCheck extends BaseTest {
             "Gomel,BLR",
             "Grodno,BLR"})
     public void temperatureCitiesTest(String nameCity) throws JsonProcessingException {
-        double controlTemperature = 35.0;
 
         ResponseEntity<String> getRequestForecast = weatherClient.getForecast(nameCity);
         Assert.assertEquals(getRequestForecast.getStatusCode().value(), 200);
 
         //parsing request forecast
         JsonNode getForecast = new ObjectMapper().readTree(getRequestForecast.getBody());
-
         //get the size of the checked blocks in the list
         int getSizeList = getForecast.get("list").size();
         //check each block of the list for a condition and perform a check
         for (int i = 0; i <= getSizeList - 1; i++) {
             String dataFromDtTxt = getForecast.get("list").get(i).path("dt_txt").toString();
             if (dataFromDtTxt.contains("15:00:00")) {
+                double controlTemperature = 35.0;
                 double valueFeelsLike = getForecast.get("list").get(i).get("main").path("feels_like").asDouble();
                 softAssertions.assertThat(valueFeelsLike < controlTemperature).isEqualTo(true);
             }
