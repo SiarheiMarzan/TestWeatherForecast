874b7a930341336dd221e4e94b5d6231ba4115be
Siarhei Marzan <siarhei.marzan@gmail.com>
2022-01-09 19:09:24 +0300

    code: added check getEnv for TestDataReader

--- src/test/java/util/TestDataReader.java
+++ src/test/java/util/TestDataReader.java
@@ -3,9 +3,17 @@ package util;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
-    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getenv("environment"));
+    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(getEnv("environment"));
 
     public static String getTestData(String key) {
         return resourceBundle.getString(key);
+    }
+
+    public static String getEnv(String name) {
+        String value = System.getenv(name);
+        if (value == null) {
+            value = System.getenv(name);
+        }
+        return value;
     }
 }
